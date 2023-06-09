518c134df89799624ab25328fb7d1fbb42593764
Bulat Valiakhmetov <61677060+vbucode@users.noreply.github.com>
2023-06-10 00:16:59 +0500

    Delete target directory

--- target/debug/deps/rust_apps-6c16a9bc9cd9c202.d
+++
@@ -1,5 +0,0 @@
-/home/bulat/Rust/rust-apps/target/debug/deps/rust_apps-6c16a9bc9cd9c202: src/main.rs
-
-/home/bulat/Rust/rust-apps/target/debug/deps/rust_apps-6c16a9bc9cd9c202.d: src/main.rs
-
-src/main.rs:
--- target/debug/rust-apps.d
+++
@@ -1,1 +0,0 @@
-/home/bulat/Rust/rust-apps/target/debug/rust-apps: /home/bulat/Rust/rust-apps/src/main.rs
