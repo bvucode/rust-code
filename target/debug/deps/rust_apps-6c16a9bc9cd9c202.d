7575352eca0b1ef9ca60cde6af845a7f7a0df288
vbucode <bulatv2@gmail.com>
2023-06-10 00:10:27 +0500

    rust-apps

---
+++ target/debug/deps/rust_apps-6c16a9bc9cd9c202.d
@@ -0,0 +1,5 @@
+/home/bulat/Rust/rust-apps/target/debug/deps/rust_apps-6c16a9bc9cd9c202: src/main.rs
+
+/home/bulat/Rust/rust-apps/target/debug/deps/rust_apps-6c16a9bc9cd9c202.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust-apps.d
@@ -0,0 +1,1 @@
+/home/bulat/Rust/rust-apps/target/debug/rust-apps: /home/bulat/Rust/rust-apps/src/main.rs
